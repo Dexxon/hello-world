e3f5a1e64db0e55261b8c88c63349d9200345a1f
Constantin <Constantin@Constantin-PC>
2014-05-06 21:10:54 +0200

    Added output 2

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World");
-		System.out.println("Output 1");
+		System.out.println("Output 2");
 	}
 
 }
